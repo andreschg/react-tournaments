f962f608a25dd7bd29f9577f7a5df0a5c7dda7b3
Oscar Chaves <andres1009_93@hotmail.com>
2022-11-28 20:56:28 -0600

    Adding simple dashboard with dummy users page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Navbar from './components/Navbar';
+import Users from './content/Users/Users';
+
 import './App.css';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <ThemeProvider theme={darkTheme} >
+        <Navbar />
+        <Users />
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/firebase/firebase.ts
@@ -0,0 +1,23 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+// import dotenv from "dotenv"
+
+declare let process: any;
+// dotenv.config();
+
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
+};
+
+console.log(process.env);
+console.log(config);
+
+const firebaseApp = initializeApp(config);
+export const firestore = getFirestore(firebaseApp);
+
+export { firebaseApp };
